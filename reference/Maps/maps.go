90976a158e76a54137b1fbd7aaffc0b398982220
Fabio Di Carlo <fabiodicarlo96@gmail.com>
2018-08-24 15:23:54 +0200

    Aggiunte funzioni per limitare mappa e get ostacoli in tutte le direzioni

--- reference/Maps/maps.go
+++ reference/Maps/maps.go
@@ -6,6 +6,10 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"time"
 )
 
 /* Dimensione mappa in quadrati da 10cm  */
@@ -16,6 +20,18 @@ const dimensionSquare = 10
 var maps [dimensionMap][dimensionMap]int
 var currentPositionX int
 var currentPositionY int
+
+/* Coordinate ostacoli */
+type coordObstacle struct {
+	north     int
+	south     int
+	east      int
+	west      int
+	northWest int
+	southWest int
+	northEast int
+	southEast int
+}
 
 /* Inizializzazione mappa con misure */
 func initMap() {
@@ -33,43 +49,104 @@ func setPositiontRobot(x int, y int) {
 	currentPositionX = x
 	currentPositionY = y
 
+	checkMapLimit()
+
+	//DA CANCELLARE
 	maps[currentPositionX][currentPositionY] = 0
 }
 
 /* Check limite mappa */
 func checkMapLimit() {
 
-	if currentPositionX > dimensionMap {
-
-		for i := 0; i < dimensionMap; i++ {
-			for j := 1; j < dimensionMap; j++ {
-				maps[i][j-1] = maps[i][j]
-			}
-		}
-	}
-
-	if currentPositionY > dimensionMap {
+	if currentPositionX > dimensionMap-1 {
 
 		for i := 1; i < dimensionMap; i++ {
 			for j := 0; j < dimensionMap; j++ {
-				maps[i-1][j] = maps[i][j]
+
+				if i == dimensionMap-1 {
+					maps[i-1][j] = maps[i][j]
+					maps[dimensionMap-1][j] = dimensionSquare
+				} else {
+					maps[i-1][j] = maps[i][j]
+				}
 			}
 		}
+
+		currentPositionX--
+	}
+
+	if currentPositionY > dimensionMap-1 {
+
+		for i := 0; i < dimensionMap; i++ {
+			for j := 1; j < dimensionMap; j++ {
+
+				if j == dimensionMap-1 {
+					maps[i][j-1] = maps[i][j]
+					maps[i][dimensionMap-1] = dimensionSquare
+				} else {
+					maps[i][j-1] = maps[i][j]
+				}
+			}
+		}
+
+		currentPositionY--
 	}
 
 	if currentPositionX < 0 {
 
+		for i := dimensionMap - 1; i > 0; i-- {
+			for j := dimensionMap - 1; j >= 0; j-- {
+
+				if i == 1 {
+					maps[i][j] = maps[i-1][j]
+					maps[0][j] = dimensionSquare
+				} else {
+					maps[i][j] = maps[i-1][j]
+				}
+			}
+		}
+
+		currentPositionX++
 	}
 
 	if currentPositionY < 0 {
 
+		for i := dimensionMap - 1; i >= 0; i-- {
+			for j := dimensionMap - 1; j > 0; j-- {
+
+				if i == 0 {
+					maps[i][j] = maps[i][j-1]
+					maps[0][j-1] = dimensionSquare
+				} else {
+					maps[i][j] = maps[i][j-1]
+				}
+			}
+		}
+
+		currentPositionY++
 	}
 }
 
 /* Set ostacolo */
-func obstacle(obstacleCm int) {
+func setObstacle(obstacleCm int) {
 
 	maps[currentPositionX][currentPositionY] = obstacleCm
+}
+
+/* Get ostacoli su posizione robot */
+func getObstacle() coordObstacle {
+
+	coord := coordObstacle{
+		north:     maps[currentPositionX-1][currentPositionY],
+		south:     maps[currentPositionX+1][currentPositionY],
+		east:      maps[currentPositionX][currentPositionY+1],
+		west:      maps[currentPositionX][currentPositionY-1],
+		northWest: maps[currentPositionX-1][currentPositionY-1],
+		southWest: maps[currentPositionX+1][currentPositionY-1],
+		northEast: maps[currentPositionX-1][currentPositionY+1],
+		southEast: maps[currentPositionX+1][currentPositionY+1]}
+
+	return coord
 }
 
 /* Set movimento NORD */
@@ -123,6 +200,10 @@ func moveSouthWest() {
 /* Stampa mappa */
 func printMap() {
 
+	cmd := exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+
 	for i := 0; i < dimensionMap; i++ {
 
 		fmt.Printf("\n")
@@ -144,13 +225,38 @@ func main() {
 	initMap()
 	setPositiontRobot(int(dimensionMap/2), int(dimensionMap/2))
 
-	for i := 0; i < 5; i++ {
+	for i := 0; i < 10000; i++ {
 
-		moveNorth()
+		switch rand.Intn(8-1) + 1 {
+		case 1:
+			moveNorth()
+			break
+		case 2:
+			moveSouth()
+			break
+		case 3:
+			moveEast()
+			break
+		case 4:
+			moveWest()
+			break
+		case 5:
+			moveNorthWest()
+			break
+		case 6:
+			moveNorthEast()
+			break
+		case 7:
+			moveSouthWest()
+			break
+		case 8:
+			moveSouthEast()
+			break
+		}
+
 		printMap()
+		fmt.Printf("OSTACOLI: %d \n", getObstacle())
 
-		//time.Sleep(500 * time.Millisecond)
+		time.Sleep(200 * time.Millisecond)
 	}
-
-	printMap()
 }
